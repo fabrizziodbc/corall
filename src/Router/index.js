026ea1666f8cf4832d69e2fa7d0ee96c35f9ed55
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-10-15 15:27:53 -0500

    Scroll to top, hover

--- src/Router/index.js
+++ src/Router/index.js
@@ -4,9 +4,11 @@ import { ContactPage } from "../containers/ContactPage";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Layout } from "../components/Layout";
 import { Solutions } from "../containers/Solutions";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Router = () => (
   <BrowserRouter>
+    <ScrollToTop />
     <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
