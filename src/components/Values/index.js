3d229ecb8d1502330db8e747b298f8d883e7176a
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-10-16 08:49:20 -0500

    about fixes

--- src/components/Values/index.js
+++ src/components/Values/index.js
@@ -1,9 +1,16 @@
-import "./style.scss"
+import { Mobile } from "../../mediaQueries/Mobile";
+import { NotMobile } from "../../mediaQueries/NotMobile";
+import "./style.scss";
 const Values = (props) => {
-    return(
-        <ul className="values-list">
-            {props.children}
-        </ul>
-    )
+  return (
+    <>
+      <Mobile>
+        <ul className="values-list">{props.children}</ul>
+      </Mobile>
+      <NotMobile>
+        <ul className="values-list values-list-b">{props.children}</ul>
+      </NotMobile>
+    </>
+  );
 };
-export {Values}
+export { Values };
